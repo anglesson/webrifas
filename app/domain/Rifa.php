5cb62968044134e4425b3e5e077ea28efd5a37d8
Anglesson Araujo <anglesson@hotmail.com>
2021-12-01 13:44:41 -0300

    feat: add Rifa class

---
+++ app/domain/Rifa.php
@@ -0,0 +1,31 @@
+<?
+
+class Rifa {
+  
+  private string $nome;
+  private string $descricao;
+  private string $preco;
+  private string $dataDoSorteio;
+  private string $tipoDaRifa;
+  private string $status;
+
+  public function criarRifa(Type $var = null)
+  {
+    # code...
+  }
+
+  public function editarRifa(Type $var = null)
+  {
+    # code...
+  }
+
+  public function excluirRifa(Type $var = null)
+  {
+    # code...
+  }
+
+  public function gerarBilhetes(int $qtd)
+  {
+    # code...
+  }
+}
